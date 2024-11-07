b89ea92a32a2a0940f33608bcd56f1d639e9d01c
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2024-11-07 11:51:45 +0800

    homePage init

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,15 @@
 <template lang="pug">
-div
-  a 123
+  Card
+    a carousel
 </template>
+
+<script setup>
+import { ref, reactive, onMounted, watch, computed, defineComponent, defineExpose } from 'vue'
+import Card from '../../components/Card.vue'
+defineComponent({
+  components: { Card },
+})
+defineExpose({
+})
+
+</script>
