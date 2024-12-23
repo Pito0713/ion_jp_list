013130883c3fc39b7f10fb8e27f28527aa422f88
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2024-12-23 14:43:43 +0800

    調整 Modal 可依判端調整背景顏色

--- store/modalStore.js
+++ store/modalStore.js
@@ -3,6 +3,7 @@ export const modalStore = defineStore('modalStore', {
     isModal: false,
     modalText: null,
     modalLoading: false,
+    modalStatus: 0,
   }),
   actions: {
     /*
@@ -10,15 +11,17 @@ export const modalStore = defineStore('modalStore', {
       需要訪問 state 需透過 this , 若不加 this
       javascript 會視為 在 ModalShow 內部的局部變數會找不值
     */
-    ModalShow(_text) {
+    ModalShow(_text, _status) {
       this.isModal = true
       this.modalText = _text
+      this.modalStatus = _status
       if (!this.modalLoading) {
         this.modalLoading = true
         setTimeout(() => {
           this.isModal = false
           this.modalText = null
           this.modalLoading = false
+          this.modalStatus = 0
         }, 2000)
       }
     }
