9146ee3f21f8025633523de550959d705249bbb9
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-01-17 17:45:45 +0800

    修正 route bug

--- middleware/route_middleware.global.js
+++ middleware/route_middleware.global.js
@@ -9,15 +9,17 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     3. 透過 auth 是否 導向  HomePage 跟 LogInPage.
     4. 控制 navState 在頁面 navbar 顯示.
   */
-  const nuxtApp = useNuxtApp()
-  const i18n = useCookie('i18n_redirected_ion')?.value || 'zhTW'  // default i18n
   const authState = useState('authState') // 定義 authState 
   const navState = useState('navState', () => null); // 定義 navState, 並預設初始值 null 
 
+  // ----- Cookie value
+  const i18n = useCookie('i18n_redirected_ion')?.value || 'zhTW'  // default i18n
   const userToken = useCookie('userToken')?.value;
 
+  // ----- Router value
+  // 以下對 router 進行分類, 兩部分 unAuthRoutes, AuthRoutes
   const allRouter = useRouter().getRoutes().map(route => { return route.name }) // get all routes
-  /* flatMap 功能類似 map 但返回結果是扁平化處理的值 */
+  /* flatMap 功能類似 map return 結果 並且進行 reduce 扁平化處理的值 */
   const unAuth = ['LogInPage', "index"] // unauth routes
   const unAuthRoutes = [...new Set(
     unAuth.flatMap((e) => { return allRouter.filter(route => route.includes(e)) })
@@ -26,8 +28,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const AuthRoutes = [...new Set(
     Auth.flatMap((e) => { return allRouter.filter(route => route.includes(e)) })
   )];
-
-  // 由 client side 進行判斷渲染
 
   // Condition: 有auth token 但是導向無權限的 route
   if (unAuthRoutes.includes(to.name)) {
@@ -38,8 +38,8 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     }
   }
 
+  // Condition: 無 auth token 但是導向 有權限 route
   if (AuthRoutes.includes(to.name)) {
-    // Condition: 無 auth token 但是導向 有權限 route
     // if (!authState.value?.token) {
     //   navState.value = { nav: false }; // set navbar useState to false
     //   // 非法轉跳登入頁
@@ -47,13 +47,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     // }
   }
 
-  // console.log(authState.value?.token, [`/${i18n}`].includes(to.name), nuxtApp.payload.serverRendered, allRouter.includes(to.name))
-  // console.log(to.name, 'to.name', allRouter.includes(to.name))
-  // 不包含在專案內 route 進行 auth 判斷轉跳
-  // if (import.meta.client && nuxtApp.payload.serverRendered && nuxtApp.isHydrating) {
-  if ([`/${i18n}`].includes(to.name) || !allRouter.includes(to.name)) {
+  // Condition: 不包含在專案內 route 進行 auth 判斷轉跳
+  if ([`index___${i18n}`].includes(to.name) || !allRouter.includes(to.name)) {
     if (userToken) return navigateTo(`/${i18n}/HomePage`)
     else return navigateTo(`/${i18n}/LogInPage`)
   }
-  // }
 })
