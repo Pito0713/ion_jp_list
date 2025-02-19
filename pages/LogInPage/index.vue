069d276e5f24797973241e3b0fa2ac6669ae63f3
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-02-19 16:40:31 +0800

    調整 log 版面新增eye 與 子父子件功能導入拆分

--- pages/LogInPage/index.vue
+++ pages/LogInPage/index.vue
@@ -1,14 +1,38 @@
 <template lang="pug">
 LayoutsPage(class='h-vh flex justify-center items-center flex-col mt-40')
-  LogInUI
+  LogInUI(
+    v-model:propsAccount="account"
+    v-model:propsPassword="password"
+    :propIsShowEye="isShowEye"
+    @submit-login="onSubmit"
+    @show-eye="handleShowEye"
+  )
 </template>
 
 <script setup>
+// @自定義 UI
 import LayoutsPage from '../../layouts/LayoutsPage.vue'
 import LogInUI from './LogInUI.vue'
+// @自定義 hook
+import { useLogInHook } from './useLogInHook'
+
+// 解構 useLogInHook 
+const {
+  onSubmit,
+  account,
+  password,
+  isShowEye,
+  handleShowEye,
+} = useLogInHook()
 
 defineComponent({
   components: { LayoutsPage, LogInUI },
+})
+
+defineExpose({
+  account,
+  password,
+  onSubmit
 })
 
 </script>
