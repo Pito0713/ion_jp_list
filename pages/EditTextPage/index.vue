83e85a58802786b23d71e67adf1f910d767dc6f8
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-02-21 15:01:04 +0800

    調整 editPage 主頁 props 導入方式

--- pages/EditTextPage/index.vue
+++ pages/EditTextPage/index.vue
@@ -1,11 +1,59 @@
 <template lang="pug">
 LayoutsPage(class='mb-16')
-  EditTextUI
+  EditTextUI(
+    v-model:propsTextInput='textInput'
+    v-model:propsTextHiraganaInput='textHiraganaInput'
+    v-model:propsTextTransInput='textTransInput'
+    v-model:propsTagArray='tagArray'
+    v-model:propsTransInput='transInput'
+    @select-tag='handleTag'
+    @add-input='handleAddInput'
+    @remove-input='handleRemoveInput'
+    @delete-text='handleDelete'
+    @submit-text='onSubmit'
+    :propsIsSubmit='isSubmit'
+    :propsTagArray='tagArray'
+    :propsInputs='inputs'
+  )
 </template>
 <script setup>
 // @自定義 UI
 import LayoutsPage from '../../layouts/LayoutsPage.vue'
 import EditTextUI from './EditTextUI.vue'
+// @自定義 Hook
+import { useEditTextHook } from './useEditTextHook'
+
+// 解構 useEditTextHook 
+const {
+  textInput,
+  textHiraganaInput,
+  textTransInput,
+  tagArray,
+  transInput,
+  inputs,
+  isSubmit,
+  onSubmit,
+  handleTag,
+  handleAddInput,
+  handleRemoveInput,
+  handleDelete,
+} = useEditTextHook()
+
+defineExpose({
+  tagArray,
+  isSubmit,
+  transInput,
+  inputs,
+  textInput,
+  textHiraganaInput,
+  textTransInput,
+  onSubmit,
+  handleTag,
+  handleAddInput,
+  handleRemoveInput,
+  handleDelete,
+})
+
 
 defineComponent({
   components: { LayoutsPage, EditTextUI },
