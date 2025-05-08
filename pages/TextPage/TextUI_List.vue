4a5fdae727c8b7669264283ce62c60c95e9a7854
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-05-08 10:20:29 +0800

    調整 textPage 樣式

--- pages/TextPage/TextUI_List.vue
+++ pages/TextPage/TextUI_List.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div(class='relative flex justify-center items-center flex-col pb-16' id='scroll-container')
-    div(class='w-full flex h-28 mb-2 mt-2 md:mt-4' id='topUpElement')
+    div(class='w-full flex h-32 mb-2' id='topUpElement')
     div(v-if='propIsTopUP' class='fixed w-10 h-10 bottom-20 right-6 md:right-1/2 md:translate-x-72' @click='$emit("scroll-To")')
       ImageFC(src='/img/topup_arrow.png' :width='35' :height='20')
     div(class='fixed w-12 h-12 bottom-4 right-6 md:right-1/2 md:translate-x-72')
@@ -8,14 +8,14 @@
         ImageFC(src='/img/add_text_circle.png' :width='50' :height='50')
     template(v-for='(item, index) in propList.data' :key='item._id')
       Card(class='w-full my-1 flex-col' )
-        div(class='w-full')
+        div(class='w-full ml-1')
           template(v-if='item.tags.length > 0')
             div(class='flex flex-row')
               template(v-for='(item, index) in item.tags' :key='item')
                 a(class='mr-2 mb-1 text-gray-400 text-sm') {{$t(item)}}
-        div(class='w-full flex flex-row ')
+        div(class='w-full flex flex-row')
           div(class='w-9/12 flex flex-row justify-start items-center mb-1')
-            a(class=' text-2xl font-medium mr-2 ') {{item.file}}
+            a(class=' text-2xl font-medium mr-2 ml-1') {{item.file}}
             a(v-if='item.fileHiragana' class=' text-gray-500 text-xs font-medium mr-1 mt-3 ') {{item.fileHiragana}}
             a(v-if='item.fileTranslate' class=' text-base font-medium ml-3 mr-1 mt-2 ') {{item.fileTranslate}}
             div(@click='$emit("copy-text",item.file)'  class='active:opacity-20 mt-2')
@@ -26,14 +26,16 @@
             div(@click='$emit("show-top", item)' class='active:opacity-40')
               ImageFC(v-if='item.isShowTop' src='/img/heart.png' :width='22.5' :height='22.5' )
               ImageFC(v-else src='/img/heart_line.png' :width='22.5' :height='22.5')
-        a(class='text-textSecond text-gray-500 text-l') {{item.translation}}
-        template(v-if='item.inputs.length > 0')
-          div(class='border-b-2 my-2.5' )
-        template(v-if='item.inputs.length > 0')
-          div(v-for='(input, index) in item.inputs' :key='index')
-            div
-              a(class='mr-2 font-medium text-lg') {{input.jpValue}}
-              a(class='text-textSecond text-gray-500 text-sm') {{input.chValue}}
+        div(class='border-l-4 border-stone-400')
+          div(class='bg-stone-100 p-2 flex justify-center items-starts flex-col')
+            a(class='text-textSecond text-gray-700 text-lg') {{item.translation}}
+            template(v-if='item.inputs.length > 0')
+              div(class='border-b-2 border-stone-200 my-1' )
+            template(v-if='item.inputs.length > 0' class='mt-2' )
+              div(v-for='(input, index) in item.inputs' :key='index')
+                div(class='h-6')
+                  a(class='mr-2 font-medium text-sm') {{input.jpValue}}
+                  a(class='text-textSecond text-gray-500 text-sm') {{input.chValue}}
     div(class='w-full flex justify-center items-center mt-4 mr-6' id='infiniteScrollElement')
     template(v-if='propList.data.length > 0')
       template(v-if='propTotalCount > propList.data.length && propHasMoreData') 
--- pages/TextPage/TextUI_Search.vue
+++ pages/TextPage/TextUI_Search.vue
@@ -1,35 +1,31 @@
 <template lang="pug">
-Card(class='custom-container flex-col fixed top-14 left-15 z-40 mt-3 pb-4')
-  div(class='flex flex-row w-full' )
-    div(class='flex border-2 border-primary-color rounded-md justify-between w-32 flex-1')
+  Card(class='custom-container flex-col mt-3 fixed top-14 left-15 z-40')
+    div(class='flex flex-row w-full' )
       input(
         :placeholder="$t('please_enter_word')" 
         :value="propTextInput"  
-        class=" border-none outline-none w-32 flex-1"
+        class="flex-1"
         @input="$emit('update:propTextInput', $event.target.value)"
       )
-      div(@click='$emit("init-input")')
-        ImageFC(src='/img/one_click.png' :width='40' :height='40')
-    LoadingBN(
-      class='w-18 flex-3'
-      :text="'search'"
-      :customClass="'mx-2 px-5'"
-      @click='$emit("init-Search")'
-    )
-  div(class='flex w-full justify-between')
-    div(class='flex')
-      template(v-for='(item, index) in propTagArray' :key='item')
-        Tag(@click='$emit("select-Tag", index)' :class='item.active && "bg-primary-color text-white"')
-          a(class='text-xs md:text-base' :class='item.active && "bg-primary-color text-white"') {{$t(item.name)}}
-    select(
-      name='sort' 
-      id='sort'
-      :value='propSelect'
-      class='flex border-2 w-30 rounded mt-2 mr-2 pl-1' 
-      @change="$emit('update:propSelect', $event.target.value)"
-    )
-      option(v-for="item in SELECTIONS" :key="item.id" :value="item.name") {{ $t(item.name) }}    
-</template>
+      LoadingBN(
+        :text="'search'"
+        :customClass="'mx-2 px-5 flex-6'"
+        @click='$emit("init-Search")'
+      )
+    div(class='flex w-full justify-between')
+      div(class='flex')
+        template(v-for='(item, index) in propTagArray' :key='item')
+          Tag(@click='$emit("select-Tag", index)' :class='item.active && "bg-primary-color text-white"')
+            a(:class='item.active && "bg-primary-color text-white"' class='text-sm md:text-base') {{$t(item.name)}}
+      select(
+        name='sort' 
+        id='sort'
+        :value='propSelect'
+        class='flex border-2 w-38 rounded mt-2 mr-2 pl-1' 
+        @change="$emit('update:propSelect', $event.target.value)"
+      )
+        option(v-for="item in SELECTIONS" :key="item.id" :value="item.name") {{ $t(item.name) }}    
+  </template>
 
 <script setup>
 // @props
@@ -42,7 +38,6 @@ defineProps({
 // $emit 事件
 defineEmits([
   'init-Search',
-  'init-input',
   'select-Tag',
   'update:propTextInput',
   'update:propSelect'
